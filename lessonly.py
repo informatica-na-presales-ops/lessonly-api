123274468d485af04d8a0e2c55f862d9ab4a00f6
William Jackson <william@subtlecoolness.com>
2022-02-03 14:06:54 -0600

    Add LessonlyClient.assignments

--- lessonly.py
+++ lessonly.py
@@ -1,3 +1,4 @@
+import datetime
 import requests.auth
 
 
@@ -6,10 +7,30 @@ class LessonlyClient:
     _base_url_v1_1 = 'https://api.lessonly.com/api/v1.1'
 
     def __init__(self, username: str, password: str):
+        self.assignments_updated_at_filter = datetime.date(2000, 1, 1)
         self.username = username
         self.password = password
         self.session = requests.Session()
         self.session.auth = requests.auth.HTTPBasicAuth(self.username, self.password)
+
+    @property
+    def assignments(self):
+        url = f'{self._base_url_v1_1}/assignments'
+        params = {
+            'gt[updated_at]': str(self.assignments_updated_at_filter),
+            'page': 1
+        }
+        while True:
+            response = self.session.get(url, params=params)
+            response.raise_for_status()
+            data = response.json()
+            yield from data.get('assignments')
+            if params.get('page') < data.get('total_pages'):
+                params.update({
+                    'page': params.get('page') + 1
+                })
+            else:
+                break
 
     def get_lesson(self, lesson_id: int) -> dict:
         response = self.session.get(f'{self._base_url_v1_1}/lessons/{lesson_id}')
