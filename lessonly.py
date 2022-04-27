467fb39d9d842b2c9dc351f2c9f0a7c5602cf663
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-04-27 14:10:48 -0500

    Get more assignments per page

--- lessonly.py
+++ lessonly.py
@@ -1,6 +1,8 @@
 import datetime
+import logging
 import requests.auth
 
+log = logging.getLogger(__name__)
 
 class LessonlyClient:
     _base_url_v1 = 'https://api.lessonly.com/api/v1'
@@ -14,18 +16,23 @@ class LessonlyClient:
         self.session.auth = requests.auth.HTTPBasicAuth(self.username, self.password)
 
     @property
-    def assignments(self):
+    def assignments(self) -> list[dict]:
         url = f'{self._base_url_v1_1}/assignments'
         params = {
             'gt[updated_at]': str(self.assignments_updated_at_filter),
-            'page': 1
+            'page': 1,
+            'per_page': 200,
         }
         while True:
             response = self.session.get(url, params=params)
             response.raise_for_status()
             data = response.json()
             yield from data.get('assignments')
-            if params.get('page') < data.get('total_pages'):
+
+            current_page = params.get('page')
+            total_pages = data.get('total_pages')
+            log.debug(f'Done getting assignments on page {current_page} of {total_pages}')
+            if current_page < total_pages:
                 params.update({
                     'page': params.get('page') + 1
                 })
